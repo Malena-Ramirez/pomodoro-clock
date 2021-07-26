8bedbe148335d6a41cf241de1e87c18ea11237bd
Malena Ramirez <maramirezuribe@gmail.com>
2021-07-26 15:07:52 -0500

    estilos en sesion y break

--- src/components/LabelStyled.js
+++ src/components/LabelStyled.js
@@ -35,16 +35,18 @@ export const LabelLengthContainer = styled.div`
   grid-template-columns: 1fr 1fr;
   row-gap: 5px;
   column-gap: 3px;
+  justify-items: center;
 `;
 
 export const DisplayLength = styled.p`
   grid-column: 1/3;
-  font-size:1.5rem;
+  font-size:1.8rem;
   text-align:center;
   margin: 0;
   color: #28115c;
   background-color: #e7dcff91;
   border-radius: 5px;
+  width: 50px;
 `;
 
 export const UpdateLengthIcon = styled.i`
